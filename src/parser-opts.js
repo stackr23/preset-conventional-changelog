a33984652c0cd729b07c2281170ab83a203e0084
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2021-05-17 12:33:28 +0200

    add gitmoji headerPattern
    may be used in coming features

--- src/parser-opts.js
+++ src/parser-opts.js
@@ -2,11 +2,9 @@
 
 module.exports = {
   headerPattern:        /^(\w*)(?:\((.*)\))?: (.*)$/,
-  headerCorrespondence: [
-    'type',
-    'scope',
-    'subject',
-  ],
+  // adaption for emoji-commits
+  // headerPattern:        /^(?::\w*:)\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
+  headerCorrespondence: [ 'type', 'scope', 'subject', 'ticket' ],
   noteKeywords:         [ 'BREAKING CHANGE' ],
   revertPattern:        /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
   revertCorrespondence: [ 'header', 'hash' ],
